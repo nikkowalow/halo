4e539e357e44495f7b7deb875f39e84522ceb981
nikkowalow <nikko@kowalow.com>
2024-10-08 20:50:49 -0400

    core: multi threading services

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,9 +1,21 @@
 use colored::*;
 use mu;
 use omicron;
+use std::thread;
 
 fn main() {
-    println!("{}", "Initialzing services...".green().bold());
-    mu::run().expect("mu failed");
-    omicron::run().expect("omicron failed");
+    println!("{}", "Initializing services...".green().bold());
+
+    let mu_thread = thread::spawn(|| {
+        mu::run().expect("mu failed");
+    });
+
+    let omicron_thread = thread::spawn(|| {
+        omicron::run().expect("omicron failed");
+    });
+
+    mu_thread.join().unwrap();
+    omicron_thread.join().unwrap();
+
+    println!("{}", "shutting down system.".red().bold());
 }
